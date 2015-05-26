ebb23065576f01445dc7e2530f48e6c0723b97ab
Ben Kuhl <bkuhl@indatus.com>
2015-05-25 23:40:22 -0400

    Added seasons

---
+++ app/Competition/SeasonCalendar.php
@@ -0,0 +1,34 @@
+<?php namespace BibleBowl\Competition;
+
+use Config;
+use Carbon\Carbon;
+
+class SeasonCalendar
+{
+    /**
+     * Get the date the current season should end
+     *
+     * @return Carbon
+     */
+    public function endDate()
+    {
+        $seasonEndDate = Carbon::now(Config::get('biblebowl.season.end'))->startOfDay();
+
+        // if August or later, end date represents next year
+        if ($seasonEndDate->format('m') >= 8) {
+            return $seasonEndDate->addYear();
+        }
+
+        return $seasonEndDate;
+    }
+
+    /**
+     * Get the date the next season should begin
+     *
+     * @return Carbon
+     */
+    public function startDate()
+    {
+        return $this->endDate();
+    }
+}
