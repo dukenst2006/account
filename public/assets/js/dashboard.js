65ed1f3eedfac85392f49c5dac131aa862f88bab
Alex Knutson <alexknutson@gmail.com>
2015-07-11 19:10:21 -0500

    Refactor Click Copy event handler. Clicking the input should trigger the button click which copies the URL and does it with style.

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -5,7 +5,30 @@ function copyToClipboard(el) {
 }
 
 $(document).ready(function () {
-    $(document).on('click', 'input.click-copy', function (e) {
-        copyToClipboard(e.target);
+
+    /**
+     * Click Copy Event.
+     */
+    $('input.click-copy, .btn-copy').on('click', function(e) {
+
+        // Trigger the btn-copy if the input is clicked.
+        if ($(e).hasClass('click-copy')) {
+            $('.btn-copy').trigger('click');
+        }
+        else {
+            // Copy the registration link into the users clipboard.
+            copyToClipboard($('input.click-copy'));
+
+            // Change the "Copy" icon into a "Checkmark" to indicate success.
+            $icon = $('.btn-copy i');
+            $icon.fadeOut(50, function(){
+                $(this).removeClass('fa-paste')
+                    .addClass('fa-check')
+                    .fadeIn(200)
+                    .animate({top: 0}, 200);
+                $(this).siblings('span')
+                    .text('Copied!');
+            });
+        }
     });
 });
