85f0c44a71355d4169ecd6ec5fab379284d94ae1
Ben Kuhl <benkuhl@gmail.com>
2016-01-08 20:11:49 -0500

    Added touch support for teams and the latest compiled teamsetjs

--- resources/views/teamset/edit-teams.blade.php
+++ resources/views/teamset/edit-teams.blade.php
@@ -1,6 +1,3 @@
-<div id="touch-warning" class="alert text-center">
-    Adjusting teams on is not supported by most tablets and phones
-</div>
 <div class="row">
     <div id="roster" class="col-md-2 col-sm-3 col-xs-6 m-t-10">
         <h5 class="text-center">Players</h5>
