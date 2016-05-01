1295106b88d5e4381cc1fd158f8aec4f767900b4
Ben Kuhl <ben.kuhl@realpage.com>
2016-05-01 07:27:04 -0400

    don't send email for 3rd party registrations

--- app/Users/Auth/SendConfirmationEmail.php
+++ app/Users/Auth/SendConfirmationEmail.php
@@ -19,16 +19,19 @@ class SendConfirmationEmail implements ShouldQueue
      */
     public function handle(User $user)
     {
-        Mail::send(
-            'emails.email-confirmation',
-            [
-                'user' => $user
-            ],
-            function (Message $message) use ($user) {
-                $message->to($user->email, $user->full_name)
-                    ->subject('Bible Bowl Account Email Confirmation');
-            }
-        );
+        // don't send email for third party registrations
+        if ($user->providers()->count() == 0) {
+            Mail::send(
+                'emails.email-confirmation',
+                [
+                    'user' => $user
+                ],
+                function (Message $message) use ($user) {
+                    $message->to($user->email, $user->full_name)
+                        ->subject('Bible Bowl Account Email Confirmation');
+                }
+            );
+        }
 
         $this->delete();
     }
