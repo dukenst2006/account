3c671d4b3f45333672a0ce728dfd112ed9b0dc81
Ben Kuhl <ben.kuhl@realpage.com>
2016-07-23 20:03:08 -0400

    More user identity

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,6 +25,18 @@
         <script type="text/javascript">
             window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
             heap.load("3821699139");
+            @if(Auth::user() != null)
+                heap.identify('{{ Auth::user()->id }}');
+                heap.addUserProperties({
+                    'Name': "{{ Auth::user()->full_name }}",
+                    'Email': "{{ Auth::user()->email }}"
+                });
+            @endif
+            @if(Session::group() != null)
+                heap.addUserProperties({
+                    'Group': "{{ Session::group()->name }}"
+            });
+            @endif
         </script>
     @endif
 </head>
