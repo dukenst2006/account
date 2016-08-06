33730c1a3c9b9cc696dad0ae81d1f0f1f206ff39
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-06 07:31:44 -0400

    Added console command

--- app/Support/Providers/ConsoleKernel.php
+++ app/Support/Providers/ConsoleKernel.php
@@ -1,5 +1,6 @@
 <?php namespace BibleBowl\Support\Providers;
 
+use BibleBowl\Location\GroupMapUpdater;
 use BibleBowl\Seasons\NotifyOfficeOfOutstandingRegistrationPayments;
 use BibleBowl\Seasons\SeasonRotator;
 use Illuminate\Console\Scheduling\Schedule;
@@ -17,6 +18,7 @@ class ConsoleKernel extends \Illuminate\Foundation\Console\Kernel
         SeasonRotator::class,
         RemindGroupsOfPendingRegistrationPayments::class,
         NotifyOfficeOfOutstandingRegistrationPayments::class,
+        GroupMapUpdater::class,
     ];
 
     /**
