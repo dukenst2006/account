df8173ddf3652693b76a480d23c875966319cd27
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-06 09:40:43 -0400

    mailto/tel links for map

--- resources/views/wordpress-map-tooltip.blade.php
+++ resources/views/wordpress-map-tooltip.blade.php
@@ -1,5 +1,5 @@
 <strong>{{ $group->name }}</strong>
 <br/><br/>
 {{ $group->owner->full_name }}<br/>
-{{ $group->owner->email }}<br/>
-{{ $group->owner->phone }}
+<a href="mailto:{{ $group->owner->email }}">{{ $group->owner->email }}</a><br/>
+<a href='tel:+1{{ $group->owner->phone }}'>{{ HTML::formatPhone($group->owner->phone) }}</a>
