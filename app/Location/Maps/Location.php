ee9554eefa387b2cd20b6a87f9370e0c9f1ed32a
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-06 09:37:19 -0400

    Scheduling map to update daily

--- app/Location/Maps/Location.php
+++ app/Location/Maps/Location.php
@@ -3,10 +3,16 @@
 namespace BibleBowl\Location\Maps;
 
 use BibleBowl\Group;
+use BibleBowl\Program;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
+    const TEEN_GROUPS_MARKER = 1;
+    const BEGINNER_GROUPS_MARKER = 2;
+    const TEEN_LEAGUES_MARKER = 3;
+    const BEGINNER_LEAGUES_MARKER = 4;
+
     protected $connection = 'wordpress';
     protected $table = 'nbwp_map_locations';
     protected $primaryKey = 'location_id';
@@ -29,6 +35,12 @@ class Location extends Model
             'guid' => $group->guid
         ];
 
+        if ($group->program_id == Program::TEEN) {
+            $this->location_group_map = self::TEEN_GROUPS_MARKER;
+        } else {
+            $this->location_group_map = self::BEGINNER_GROUPS_MARKER;
+        }
+
         $this->location_title = $group->name;
         $this->location_address = $group->meetingAddress.'';
         $this->location_city = $group->meetingAddress->city;
@@ -50,5 +62,17 @@ class Location extends Model
     public function setLocationExtrafieldsAttribute(array $value)
     {
         $this->attributes['location_extrafields'] = serialize($value);
+    }
+
+    public function getLocationGroupMapAttribute($value) : array
+    {
+        return unserialize($value);
+    }
+
+    public function setLocationGroupMapAttribute($markerId)
+    {
+        $this->attributes['location_group_map'] = serialize([
+            $markerId
+        ]);
     }
 }
--- app/Support/Providers/ConsoleKernel.php
+++ app/Support/Providers/ConsoleKernel.php
@@ -33,6 +33,9 @@ class ConsoleKernel extends \Illuminate\Foundation\Console\Kernel
         $schedule->command(SeasonRotator::COMMAND)
             ->daily()->thenPing('https://hchk.io/c9e89191-f417-44ee-bf88-03739c439f74');
 
+        $schedule->command(GroupMapUpdater::COMMAND)
+            ->daily()->thenPing('https://hchk.io/5e3052eb-7c81-4e0f-aa34-dfcc833ed7c7');
+
         // reminders for unpaid registration fees
         $schedule->command(RemindGroupsOfPendingRegistrationPayments::COMMAND)
             ->cron('0 0 * 10,11,12,1,2,3,4,5 2 *'); // Oct-May every Tuesday
