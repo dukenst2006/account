ee05ed90446ce5a26cd7653c32135a1721f99882
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-21 08:10:23 -0400

    Added register link with group

--- resources/views/wordpress-map-tooltip.blade.php
+++ resources/views/wordpress-map-tooltip.blade.php
@@ -1,5 +1,6 @@
-<strong>{{ $group->name }}</strong>
-<br/><br/>
+<div style="font-size:120%"><strong>Covenant</strong></div>
+<div style="margin-bottom:1em;"><a href="{{ url($group->registrationReferralLink()) }}">Register with group</a></div>
+
 {{ $group->owner->full_name }}<br/>
 <a href="mailto:{{ $group->owner->email }}">{{ $group->owner->email }}</a><br/>
-<a href='tel:+1{{ $group->owner->phone }}'>{{ HTML::formatPhone($group->owner->phone) }}</a>
+<a href='tel:+1{{ $group->owner->phone }}'>{{ HTML::formatPhone($group->owner->phone) }}</a><br/><br/>
