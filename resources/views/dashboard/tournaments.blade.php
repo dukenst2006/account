8842bb151115d1f2501cebeb32f641a300abfe52
Ben Kuhl <ben.kuhl@realpage.com>
2016-08-21 14:46:57 -0400

    Removed hiding horizontal bar on mobile

--- resources/views/dashboard/tournaments.blade.php
+++ resources/views/dashboard/tournaments.blade.php
@@ -5,18 +5,16 @@
                 <h4 class="bold">Tournaments</h4>
             </div>
             <div class="grid-body no-border" style="padding-bottom:0;">
-                <table class="table" style="margin-bottom: 0">
-                    @foreach($tournaments as $tournament)
-                        <tr>
-                            <td class="v-align-middle">
-                                <a href="/tournaments/{{ $tournament->slug }}" class="bold">{{ $tournament->name }}</a>
-                            </td>
-                            <td class="v-align-middle text-center">
-                                <a href="/tournaments/{{ $tournament->slug }}/group">Manage Registration</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </table>
+                @foreach($tournaments as $tournament)
+                    <div class="row b-t b-grey p-t-5 p-b-5">
+                        <div class="col-md-7 col-sm-7 col-xs-7">
+                            <a href="/tournaments/{{ $tournament->slug }}" class="bold">{{ $tournament->name }}</a>
+                        </div>
+                        <div class="col-md-5 col-sm-5 col-xs-5 text-center">
+                            <a href="/tournaments/{{ $tournament->slug }}/group">Manage Registration</a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
