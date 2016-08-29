98ef4ce9034fc468a322fec34e867d903ce6db53
Ben Kuhl <benkuhl@gmail.com>
2016-08-28 21:06:13 -0400

    Update season-overview.blade.php

--- resources/views/dashboard/season-overview.blade.php
+++ resources/views/dashboard/season-overview.blade.php
@@ -10,7 +10,7 @@
                 <div class="clearfix"></div>
             </div>
             <div class="col-md-4 col-sm-4 text-center">
-                <a href="/admin/reports/players">
+                <a href="/admin/reports/seasons">
                     <h2 class="semi-bold text-success no-margin p-t-35 p-b-10">{{ number_format($playerCount) }}</h2>
                     <div class="tiles-title blend p-b-25">PLAYERS</div>
                 </a>
