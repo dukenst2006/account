b5a4f245b19079d4434fb6bb171f012d3d20a38d
Ben Kuhl <ben.kuhl@realpage.com>
2016-12-21 14:53:22 -0500

    Reverted test

--- tests/Account/CleanupOrphanAccountsTest.php
+++ tests/Account/CleanupOrphanAccountsTest.php
@@ -1,8 +1,6 @@
 <?php
 
 use BibleBowl\User;
-use BibleBowl\Users\CleanupOrphanAccounts;
-use Carbon\Carbon;
 
 class CleanupOrphanAccountsTest extends TestCase
 {
@@ -12,14 +10,12 @@ class CleanupOrphanAccountsTest extends TestCase
     public function onlyDeletesUnconfirmedAccounts()
     {
         $userCount = User::count();
-        $orphanedFor = Carbon::now()->subDays(config('biblebowl.cleanup-orphan-accounts-after'));
-        $shouldBeDeletedCount = User::where('status', User::STATUS_UNCONFIRMED)->where('created_at', '>', $orphanedFor)->count();
+        $shouldBeDeletedCount = User::where('status', User::STATUS_UNCONFIRMED)->count();
+        $this->assertGreaterThan(0, $shouldBeDeletedCount);
 
-        $this->assertEquals(1, $shouldBeDeletedCount);
+        $this->artisan(\BibleBowl\Users\CleanupOrphanAccounts::COMMAND);
 
-        $this->artisan(CleanupOrphanAccounts::COMMAND);
-
-        $this->assertEquals(1, User::where('status', User::STATUS_UNCONFIRMED)->count());
+        $this->assertEquals(0, User::where('status', User::STATUS_UNCONFIRMED)->count());
         $this->assertEquals($userCount - $shouldBeDeletedCount, User::count());
     }
 }
