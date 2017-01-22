50431cbd73947008e25e7e22473ff290bbfeceb1
Ben Kuhl <ben.kuhl@realpage.com>
2017-01-22 15:25:04 -0500

    Mobile-friendly rosters

--- resources/views/group/roster.blade.php
+++ resources/views/group/roster.blade.php
@@ -9,12 +9,12 @@
                 <div class="grid simple">
                     <div class="grid-title no-border">
                         <div class="row">
-                            <div class="col-md-9 p-l-20 p-t-15">
+                            <div class="col-md-9 col-sm-9 col-xs-7 p-l-20 p-t-15">
                                 <h4>Player <span class="semi-bold">Roster</span></h4>
                             </div>
-                            <div class="col-md-3 text-right p-r-20 p-t-15">
+                            <div class="col-md-3 col-sm-3 col-xs-5 text-right p-r-20 p-t-15">
                                 @if(count($active_players) > 0)
-                                    <a href="/roster/export" type="button" class="btn btn-primary btn-cons"><i class="fa fa-download"></i>&nbsp;Download CSV</a>
+                                    <a href="/roster/export" type="button" class="btn btn-info btn-cons"><i class="fa fa-download"></i>&nbsp;Download CSV</a>
                                 @endif
                             </div>
                         </div>
@@ -26,9 +26,9 @@
                             <tr>
                                 <th style="width:20%">Player</th>
                                 <th style="width:25%">Guardian</th>
-                                <th style="width:5%" class="text-center">Age</th>
-                                <th style="width:15%" class="text-center">Grade</th>
-                                <th style="width:15%" class="text-center">Shirt Size</th>
+                                <th style="width:5%" class="text-center hidden-xs">Age</th>
+                                <th style="width:15%" class="text-center hidden-xs">Grade</th>
+                                <th style="width:15%" class="text-center hidden-xs">Shirt Size</th>
                                 <th style="width:20%" class="text-center">Options</th>
                             </tr>
                             </thead>
@@ -36,13 +36,13 @@
                         @if(count($active_players) > 0)
                             @foreach($active_players as $player)
                             <tr>
-                                <td class="v-align-middle">{{ $player->last_name }}, {{ $player->first_name }}</td>
+                                <td class="v-align-middle">{{ $player->last_name }}<span class="hidden-xs">, {{ $player->first_name }}</span></td>
                                 <td class="v-align-middle"><a href="/guardian/{{ $player->guardian->id }}">{{ $player->guardian->full_name }}</a></td>
-                                <td class="v-align-middle text-center">{{ $player->age() }}</td>
-                                <td class="v-align-middle text-center">{{ \BibleBowl\Presentation\Describer::describeGradeShort($player->pivot->grade) }}</td>
-                                <td class="v-align-middle text-center">{{ $player->pivot->shirt_size }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ $player->age() }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ \BibleBowl\Presentation\Describer::describeGradeShort($player->pivot->grade) }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ $player->pivot->shirt_size }}</td>
                                 <td class="v-align-middle text-center">
-                                    <a href="/player/{{ $player->id }}/deactivate" id="deactivate-{{ $player->id }}" data-toggle="tooltip" data-placement="left" title="Use this option if this player is no longer participating this season.">Deactivate</a>
+                                    <a href="/player/{{ $player->id }}/deactivate" class="btn btn-primary btn-cons btn-small" id="deactivate-{{ $player->id }}" data-toggle="tooltip" data-placement="left" title="Use this option if this player is no longer participating this season.">Deactivate</a>
                                 </td>
                             </tr>
                             @endforeach
--- resources/views/group/roster/inactive.blade.php
+++ resources/views/group/roster/inactive.blade.php
@@ -17,22 +17,22 @@
                             <tr>
                                 <th style="width:20%">Player</th>
                                 <th style="width:25%">Guardian</th>
-                                <th style="width:5%" class="text-center">Age</th>
-                                <th style="width:15%" class="text-center">Grade</th>
-                                <th style="width:15%" class="text-center">Shirt Size</th>
+                                <th style="width:5%" class="text-center hidden-xs">Age</th>
+                                <th style="width:15%" class="text-center hidden-xs">Grade</th>
+                                <th style="width:15%" class="text-center hidden-xs">Shirt Size</th>
                                 <th style="width:20%" class="text-center">Options</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($inactive_players as $player)
                             <tr>
-                                <td class="v-align-middle">{{ $player->last_name }}, {{ $player->first_name }}</td>
+                                <td class="v-align-middle">{{ $player->last_name }}<span class="hidden-xs">, {{ $player->first_name }}</span></td>
                                 <td class="v-align-middle">{{ $player->guardian->full_name }}</td>
-                                <td class="v-align-middle text-center">{{ $player->age() }}</td>
-                                <td class="v-align-middle text-center">{{ \BibleBowl\Presentation\Describer::describeGrade($player->pivot->grade) }}</td>
-                                <td class="v-align-middle text-center">{{ $player->pivot->shirt_size }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ $player->age() }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ \BibleBowl\Presentation\Describer::describeGradeShort($player->pivot->grade) }}</td>
+                                <td class="v-align-middle text-center hidden-xs">{{ $player->pivot->shirt_size }}</td>
                                 <td class="v-align-middle text-center">
-                                    <a href="/player/{{ $player->id }}/activate" id="activate-{{ $player->id }}">Activate</a>
+                                    <a href="/player/{{ $player->id }}/activate" class="btn btn-primary btn-cons btn-small" id="activate-{{ $player->id }}">Activate</a>
                                 </td>
                             </tr>
                             @endforeach
