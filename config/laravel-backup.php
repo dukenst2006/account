f89af04775874df2bfb5b375aa574fb70256403b
Ben Kuhl <benkuhl@gmail.com>
2017-03-31 13:43:16 +0000

    Apply fixes from StyleCI

--- config/laravel-backup.php
+++ config/laravel-backup.php
@@ -102,9 +102,9 @@ return [
      */
     'monitorBackups' => [
         [
-            'name' => env('APP_URL'),
-            'disks' => ['local'],
-            'newestBackupsShouldNotBeOlderThanDays' => 1,
+            'name'                                   => env('APP_URL'),
+            'disks'                                  => ['local'],
+            'newestBackupsShouldNotBeOlderThanDays'  => 1,
             'storageUsedMayNotBeHigherThanMegabytes' => 5000,
         ],
 
