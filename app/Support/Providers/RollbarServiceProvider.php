61d602f2246e14734ccac58c36b432903af7a0eb
Ben Kuhl <ben.kuhl@realpage.com>
2017-04-02 21:17:51 -0400

    Fixed exception handling in Laravel 5.4

--- app/Support/Providers/RollbarServiceProvider.php
+++ app/Support/Providers/RollbarServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Support\Providers;
 
 use App\User;
 use Auth;
+use Illuminate\Log\Events\MessageLogged;
 use Jenssegers\Rollbar\RollbarLogHandler;
 
 class RollbarServiceProvider extends \Jenssegers\Rollbar\RollbarServiceProvider
@@ -13,19 +14,19 @@ class RollbarServiceProvider extends \Jenssegers\Rollbar\RollbarServiceProvider
         $app = $this->app;
 
         // Listen to log messages.
-        $app['log']->listen(function ($level, $message, $context) use ($app) {
+        $app['log']->listen(function (MessageLogged $message) use ($app) {
             $user = Auth::user();
 
             // log logged in user details
             if ($user instanceof User) {
-                $context['person'] = [
+                $message->context['person'] = [
                     'id'        => $user->id,
                     'username'  => $user->full_name,
                     'email'     => $user->email,
                 ];
             }
 
-            $app[RollbarLogHandler::class]->log($level, $message, $context);
+            $app[RollbarLogHandler::class]->log($message->level, $message->message, $message->context);
         });
     }
 }
