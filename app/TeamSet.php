723c581add8504f6ede0f839a724970b29729706
Ben Kuhl <benkuhl@gmail.com>
2017-04-20 22:11:23 +0000

    Apply fixes from StyleCI

--- app/TeamSet.php
+++ app/TeamSet.php
@@ -92,8 +92,8 @@ class TeamSet extends Model
         $teamIds = $this->teams->modelKeys();
 
         return Player::whereHas('teams', function ($q) use ($teamIds) {
-                $q->whereIn('id', $teamIds);
-            })
+            $q->whereIn('id', $teamIds);
+        })
             ->orderBy('last_name', 'ASC')
             ->orderBy('first_name', 'ASC');
     }
