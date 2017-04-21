f8431144d633ff3dc5b55e8a03a1bfb923143121
Ben Kuhl <ben.kuhl@realpage.com>
2017-04-20 20:00:51 -0400

    Ordering player names consistently

--- tests/Tournaments/GroupRegistrationTest.php
+++ tests/Tournaments/GroupRegistrationTest.php
@@ -339,7 +339,7 @@ class GroupRegistrationTest extends TestCase
             ->see('1 require payment');
 
         $this->assertEquals(2, $event->players()->count());
-
+dd($event->players->toArray());
         // verify previously set receipt_id isn't overwritten
         $this->assertEquals($receipt->id, $event->players->get(0)->pivot->receipt_id);
     }
